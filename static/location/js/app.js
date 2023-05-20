74317f166eee4bd0bdaa6799dabd0fede3f21c91
etali-m <etalijunior15@gmail.com>
2023-05-20 15:54:04 +0100

    new user position

--- static/location/js/app.js
+++ static/location/js/app.js
@@ -21,7 +21,7 @@ const map = new mapboxgl.Map({
 
 position_btn.addEventListener('click', function(){
    // Créer un nouveau marqueur
-    const marker = new mapboxgl.Marker({
+    /*const marker = new mapboxgl.Marker({
         color: 'red',
         scale: 0.75,
     })
@@ -34,7 +34,19 @@ position_btn.addEventListener('click', function(){
         center: [lon, lat], 
         essential: true,
         curve: 1
-    })
+    })*/
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+        positionOptions: {
+        enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+        })
+    );
+
 });
 
 //Ici on va placer sur la carte les points de localisation de l'utilisateur recherché
